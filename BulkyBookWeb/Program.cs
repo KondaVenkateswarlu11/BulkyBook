7e7280c2ad8fc54de080881ec6349574adcadeb8
venkateswarlukonda11@gmail.com <venkateswarlukonda11@gmail.com>
2023-02-03 12:45:29 +0530

    Added Roles

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -3,7 +3,8 @@ using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-
+using Microsoft.AspNetCore.Identity.UI.Services;
+using BulkyBook.Utility;
 
 internal class Program
 {
@@ -18,11 +19,12 @@ internal class Program
             builder.Configuration.GetConnectionString("DefaultConnection")
         ));
 
-        builder.Services.AddDefaultIdentity<IdentityUser>()
+        builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProviders()
             .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+        builder.Services.AddSingleton<IEmailSender,EmailSender>();
 
         builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
