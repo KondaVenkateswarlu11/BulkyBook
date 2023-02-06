6a320190c6a2b8d6363f8d6b292d2b8700245f53
venkateswarlukonda11@gmail.com <venkateswarlukonda11@gmail.com>
2023-02-06 12:36:47 +0530

    OrderVM and Retrieve all Orders

---
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		
+			private readonly IUnitOfWork _unitOfWork;
+			public OrderController(IUnitOfWork unitOfWork)
+			{
+				_unitOfWork = unitOfWork;
+			}
+
+			public IActionResult Index()
+			{
+				return View();
+			}
+
+			#region API CALLS
+			[HttpGet]
+			public IActionResult GetAll()
+			{
+				IEnumerable<OrderHeader> orderHeaders;
+				orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+				return Json(new { data = orderHeaders });
+			}
+			#endregion
+		
+	}
+}
