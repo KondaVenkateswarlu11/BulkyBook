5ba1f167e2b94d1c76051585f664db9b2e0c4b4e
venkateswarlukonda11@gmail.com <venkateswarlukonda11@gmail.com>
2023-02-13 12:06:31 +0530

    Demo - Filters In OrderList

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
@@ -21,10 +23,29 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 
 			#region API CALLS
 			[HttpGet]
-			public IActionResult GetAll()
+			public IActionResult GetAll(string status)
 			{
 				IEnumerable<OrderHeader> orderHeaders;
 				orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+
+				switch (status)
+				{ 
+					case "pending":
+					orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+						break;
+					case "inprocess":
+						orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+						break;
+					case "completed":
+						orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+						break;
+					case "approved":
+						orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+						break;
+					default:
+						break;
+				}
+
 				return Json(new { data = orderHeaders });
 			}
 			#endregion
